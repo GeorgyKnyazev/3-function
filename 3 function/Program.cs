7196dda78bb66d7d3d412781073d8ca8b5e78ca4
Георгий Князев <saint_georgy@yahoo.com>
2022-09-17 19:29:15 +0300

    Добавьте файлы проекта.

---
+++ 3 function/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _3_function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Console.Write("Ведите число: ");
+            userInput = Console.ReadLine();
+
+            ConvertToNamber(userInput);
+        }
+        static void ConvertToNamber(string varible)
+        {
+            bool resultOperation;
+            bool continueFunction = true;
+
+            while (continueFunction == true)
+            {
+                resultOperation = int.TryParse(varible, out int result);
+                if (resultOperation == true)
+                {
+                    Console.WriteLine(result);
+                    continueFunction = false;
+                }
+                else 
+                {
+                    Console.WriteLine("Вы ввели не число");
+                    Console.Write("Ведите число: ");
+                    varible = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
